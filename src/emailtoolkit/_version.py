28b2e41a2c6b02dcf55cbb531c51ac1cae8d42aa
I'm Your Boy Roy <V1x0r87@gmail.com>
2026-02-26 17:04:58 -0700

    chore: formatting pass

--- src/emailtoolkit/_version.py
+++ src/emailtoolkit/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.5.dev0+gd9ab27d53.d20260226'
-__version_tuple__ = version_tuple = (0, 1, 5, 'dev0', 'gd9ab27d53.d20260226')
+__version__ = version = "0.1.5.dev0+gd9ab27d53.d20260226"
+__version_tuple__ = version_tuple = (0, 1, 5, "dev0", "gd9ab27d53.d20260226")
 
 __commit_id__ = commit_id = None
